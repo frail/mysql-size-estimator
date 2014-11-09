bd97f4c7f19a047e811bd799a00d40c584735c18
frail <ozgur.orhan@gmail.com>
2014-11-09 14:46:42 +0200

    cli rewrite to test better

--- mse/cli.py
+++ mse/cli.py
@@ -29,14 +29,54 @@ from .base import Table
 from .charset import fix_charset_collation
 
 
-def read_file_or_stdin(in_file):
-    if in_file == "-":
+class CliParser:
+    def __init__(self, args):
+        self._parser = Parser()
+        self.table = None
+        self._parse_table(args)
+        self.report_row_sizes = [int(size) for size in args.get("--row-size")]
+        self.is_quiet = args.get('--quiet')
+
+    def _parse_table(self, args):
+        if args.get("dummy"):
+            self._create_dummy_table()
+        elif args.get("file"):
+            if args.get("INPUT") == '-':
+                self._read_table_from_stdin()
+            else:
+                self._read_table_from_file(args.get("INPUT"))
+        self._update_columns(args)
+        self._update_indexes(args)
+        self._update_charset(args)
+
+    def _create_dummy_table(self):
+        self.table = Table("dummy")
+
+    def _read_table_from_stdin(self):
         data = sys.stdin.readlines()
-        return " ".join(data)
-    else:
-        with open(in_file, "r") as f:
-            data = f.read().replace("\n", "")
-        return data
+        table_str = " ".join(data)
+        self.table = self._parser.parse_table(table_str)
+
+    def _read_table_from_file(self, filename):
+        with open(filename, "r") as f:
+            table_str = f.read().replace("\n", "")
+        self.table = self._parser.parse_table(table_str)
+
+    def _update_columns(self, args):
+        for column_string in args.get("--column"):
+            column = self._parser.parse_column(column_string)
+            self.table.add_or_update_column(column)
+
+    def _update_indexes(self, args):
+        for index_string in args.get("--index"):
+            index = self._parser.parse_index(index_string)
+            self.table.add_or_update_index(index)
+
+    def _update_charset(self, args):
+        if args.get("--charset") or args.get("--collation"):
+            charset, collation = fix_charset_collation(args.get("--charset"), args.get("--collation"))
+            self.table.charset = charset
+            self.table.collation = collation
 
 
 def make_estimations(table, row_sizes, quiet):
@@ -45,35 +85,6 @@ def make_estimations(table, row_sizes, quiet):
 
 
 def main():
-    arg = docopt(__doc__)
-
-    parser = Parser()
-
-    table = None
-
-    # ----- READ TABLE -----
-    if arg.get("dummy"):
-        table = Table("dummy")
-    elif arg.get("file"):
-        sql_string = read_file_or_stdin(arg.get('INPUT'))
-        table = parser.parse_table(sql_string)
-
-    # ----- ADD CUSTOM COLUMNS -----
-    if arg.get("--column"):
-        for column_string in arg.get("--column"):
-            column = parser.parse_column(column_string)
-            table.add_or_update_column(column)
-
-    # ----- ADD CUSTOM INDICES -----
-    for index_string in arg.get("--index"):
-        index = parser.parse_index(index_string)
-        table.add_or_update_index(index)
-
-    # ----- CHANGE DEFAULT CHARSET / COLLATION -----
-    if arg.get("--charset") or arg.get("--collation"):
-        charset, collation = fix_charset_collation(arg.get("--charset"), arg.get("--collation"))
-        table.charset = charset
-        table.collation = collation
-
-    row_sizes = [int(size) for size in arg.get("--row-size")]
-    make_estimations(table, row_sizes, arg.get('--quiet'))
+    args = docopt(__doc__)
+    parsed = CliParser(args)
+    make_estimations(parsed.table, parsed.report_row_sizes, parsed.is_quiet)
